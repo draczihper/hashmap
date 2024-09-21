481a27991fdc3678942a94d2d82a4808a9835c96
Drac Zihper <draczihper@gmail.com>
2024-09-21 12:27:48 +0300

    Add set, get, has, remove, length methods

--- script.js
+++ script.js
@@ -18,7 +18,50 @@ class HashMap {
 		return hashCode;
 	}
 
+	set(key, value) {
+        const index = this.hash(key);
+        if (!this.buckets[index].hasOwnProperty(key)) {
+            this.size++;
+        }
+        this.buckets[index][key] = value;
+
+        if (this.size / this.buckets.length > this.loadFactor) {
+            this.resize();
+        }
+    }
+
+	get(key) {
+        const index = this.hash(key);
+        return this.buckets[index].hasOwnProperty(key) ? this.buckets[index][key] : null;
+    }
+
+	has(key) {
+        const index = this.hash(key);
+        return this.buckets[index].hasOwnProperty(key);
+	}
 	
+	remove(key) {
+        const index = this.hash(key);
+        if (this.buckets[index].hasOwnProperty(key)) {
+            const value = this.buckets[index][key];
+            delete this.buckets[index][key];
+            this.size--;
+            return value;
+        }
+        return null;
+	}
+	
+	length() {
+        return this.size;
+    }
+
+    clear() {
+        this.buckets = new Array(16);
+        for (let i = 0; i < 16; i++) {
+            this.buckets[i] = {};
+        }
+        this.size = 0;
+    }
 
 }
 
